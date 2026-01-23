7b27d0fef8d2825e7991a1302ef8e02b52084f8d
shroff-yaksi <shroffyaksi@gmail.com>
2026-01-23 17:00:18 +0530

    Initial commit: Restored and structured fitness app with modern Flutter compatibility

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:fitness_flutter/core/const/color_constants.dart';
+import 'package:fitness_flutter/screens/onboarding/page/onboarding_page.dart';
+import 'package:fitness_flutter/screens/tab_bar/page/tab_bar_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  // Firebase disabled for demo - uncomment when configured
+  // await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Notifications disabled for demo
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Bypass auth for demo - set to false to see onboarding
+    final isLoggedIn = true;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fitness',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: ColorConstants.textColor),
+          bodyMedium: TextStyle(color: ColorConstants.textColor),
+        ),
+        fontFamily: 'NotoSansKR',
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: isLoggedIn ? TabBarPage() : OnboardingPage(),
+    );
+  }
+}
